4d47283f7da1712e3a6cc9174c78c598072a573a
Alesenko Elena <alesenko.e@drofa.ru>
2017-08-09 16:28:39 +0300

    new info

--- _site/bin/download.sh
+++ _site/bin/download.sh
@@ -3,6 +3,6 @@ set -o errexit
 set -o xtrace
 set -o pipefail
 
-./node_modules/.bin/gsjson 11HIARllbIOPq4FgpZbrchu80XvjRgLtVsfIbGFgP6s0 |
-	./node_modules/.bin/index-by menu |
-	./node_modules/.bin/json > _data/teachers.json
+curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=0&single=true&output=csv' > _data/clubs.csv
+curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=1367138216&single=true&output=csv' > _data/members.csv
+curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=1912095489&single=true&output=csv' > _data/champions.csv
