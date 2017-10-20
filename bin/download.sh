118a14fd867190af4d4e952364c5899f26ed5091
Alesenko Elena <omegka@gmail.com>
2017-10-20 10:21:28 +0300

    update info

--- bin/download.sh
+++ bin/download.sh
@@ -7,3 +7,4 @@ curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLc
 curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=1367138216&single=true&output=csv' > _data/members.csv
 curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=1912095489&single=true&output=csv' > _data/champions.csv
 curl 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRX1a4w77XMVQGiHO_dCejAnIwSrK-EZw4aOUC14im4q6oi5f5gEeIPt4q7A1fqh0qvdM4YJbdlxgPY/pub?gid=1068857440&single=true&output=csv' > _data/tests.csv
+curl 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRX1a4w77XMVQGiHO_dCejAnIwSrK-EZw4aOUC14im4q6oi5f5gEeIPt4q7A1fqh0qvdM4YJbdlxgPY/pub?gid=1493169956&single=true&output=csv' > _data/show-result.csv
