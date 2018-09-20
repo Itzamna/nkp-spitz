d8dd70fab70cad0a32183ff402277f2307a08280
Alesenko Elena <omegka@gmail.com>
2018-09-20 10:55:48 +0300

    update info

--- bin/download.sh
+++ bin/download.sh
@@ -8,3 +8,4 @@ curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLc
 curl 'https://docs.google.com/spreadsheets/d/1YAfqJXdUaAOm4JbQZk2X1WgjMJ9srUFKLcAkPVJbAMc/pub?gid=1912095489&single=true&output=csv' > _data/champions.csv
 curl 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRX1a4w77XMVQGiHO_dCejAnIwSrK-EZw4aOUC14im4q6oi5f5gEeIPt4q7A1fqh0qvdM4YJbdlxgPY/pub?gid=1068857440&single=true&output=csv' > _data/tests.csv
 curl 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRX1a4w77XMVQGiHO_dCejAnIwSrK-EZw4aOUC14im4q6oi5f5gEeIPt4q7A1fqh0qvdM4YJbdlxgPY/pub?gid=970927391&single=true&output=csv' > _data/showresult.csv
+curl 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRX1a4w77XMVQGiHO_dCejAnIwSrK-EZw4aOUC14im4q6oi5f5gEeIPt4q7A1fqh0qvdM4YJbdlxgPY/pub?gid=1320504325&single=true&output=csv' > _data/calendar2019.csv
